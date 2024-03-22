b54aa50f99d157a680a7b773c41b6515cc23a46d
Jonathan Spannring <jonathan.spannring@gmail.com>
2024-03-22 12:47:46 -0700

    Added optimizeDeps to vite.config. Reinstalled package-lock.json

--- ghi/vite.config.js
+++ ghi/vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react({include: '**/*.jsx'})],
+    plugins: [react({ include: '**/*.jsx' })],
+    optimizeDeps: {
+        include: ['@emotion/styled'],
+    },
     server: {
         hmr: true,
         host: true,
         strictPort: true,
         watch: {
             usePolling: true,
-        }
+        },
     },
 })
